aaafd432397dac86b99384dcfc6ff2ac6083f02c
Adrian Stanciu <astanciu@Adrians-MacBook-Pro.local>
2015-08-27 09:33:03 +0300

    First commit

---
+++ CheckedButton/CheckedButton.swift
@@ -0,0 +1,13 @@
+//
+//  CheckedButton.swift
+//  CheckedButton
+//
+//  Created by Adrian Stanciu on 8/26/15.
+//  Copyright © 2015 Adrian Stanciu. All rights reserved.
+//
+
+import Cocoa
+
+class CheckedButton: UIButton {
+
+}
